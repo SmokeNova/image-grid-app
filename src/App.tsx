52b504e3ba6eea528eab885373f1e19db25134d0
Maaz Imran <ssjbluemaaz@gmail.com>
2023-10-03 18:45:46 +0500

    Created the redux store, made a slice for images to display and created an asyncthunk to fetch images, will move to the main app next

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
 import "./App.css";
 import '@mantine/core/styles.css';
+import { useEffect } from 'react';
+import { RootState, useAppDispatch } from "./store";
+import { useSelector } from 'react-redux';
 
 import { MantineProvider } from '@mantine/core';
+import { fetchImages } from "./slices/imagesSlice";
 
 function App() {
+  const dispatch = useAppDispatch();
+  const { images, isLoading } = useSelector((state: RootState) => state.images);
+
+  useEffect(() => {
+    dispatch(fetchImages())
+  }, [])
+
+  if (isLoading) return 'loading';
+
+  console.log(images);
+
   return (
     <MantineProvider>
       <h1 className="text-emerald-500 text-3xl">Hello World</h1>
---
+++ src/slices/imagesSlice.ts
@@ -0,0 +1,44 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+interface IImage {
+    id: string;
+    url: string;
+    width: number;
+    height: number;
+}
+
+interface IImages {
+    images: IImage[],
+    isLoading: boolean,
+}
+
+const url = "https://api.thecatapi.com/v1/images/search?limit=80&mime_types=&order=Random&size=small&page=3&sub_id=demo-ce06ee"
+
+export const fetchImages = createAsyncThunk('images/fetchImages', async (_, thunkAPI) => {
+    try {
+        const res = await axios.get(url)
+        return res.data;
+    } catch(error: any) {
+        thunkAPI.rejectWithValue("something went wrong!");
+    }
+})
+
+const initialState: IImages = {
+    images: [],
+    isLoading: true,
+}
+
+const imagesSlice = createSlice({
+    name: "images",
+    initialState,
+    reducers: {},
+    extraReducers: (builder) => {
+        builder.addCase(fetchImages.fulfilled, (state, action) => {
+            state.images = [...action.payload];
+            state.isLoading = false;
+        })
+    }
+})
+
+export default imagesSlice.reducer;
